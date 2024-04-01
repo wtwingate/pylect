2517e17b022d50d6610dad8e5c5a044f9a36593d
William Wingate <wtwingate@gmail.com>
2024-04-01 19:18:38 -0400

    Start working on function to calculate liturgical day

--- src/lectionary/cli.py
+++ src/lectionary/cli.py
@@ -1,12 +1,14 @@
 from lectionary.lectionary import Lectionary
 from lectionary.psalter import Psalter
 from lectionary.esv import get_esv_text
+import datetime
 
 
 def main():
     psalter = Psalter()
     lectionary = Lectionary()
-    lessons = lectionary.get_lessons("today")
+    today = datetime.date.today()
+    lessons = lectionary.get_lessons(today)
     texts = []
     texts.append(get_esv_text(lessons[0]))  # The First Lesson
     texts.append(psalter.get_psalm(lessons[1]))  # The Psalm
--- src/lectionary/lectionary.py
+++ src/lectionary/lectionary.py
@@ -1,4 +1,7 @@
 import csv
+import calendar
+import datetime
+import dateutil
 
 
 class Lectionary:
@@ -6,19 +9,21 @@ class Lectionary:
         self.__lectionary = {}
         self.__import_lectionary()
 
-    def get_lessons(self, date) -> str:
+    def get_lessons(self, date: datetime.date) -> str:
         day, year = self.__get_liturgical_day(date)
         lessons = self.__lectionary[day][year].split("; ")
         return lessons
 
-    def __import_lectionary(self):
+    def __import_lectionary(self) -> None:
         """Import lectionary from CSV file"""
         with open("docs/sunday_lectionary.csv", newline="") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 self.__lectionary[row["DAY"]] = row
 
-    def __get_liturgical_day(self, date):
+    def __get_liturgical_day(self, today: datetime.date) -> tuple:
+        easter_sunday = dateutil.easter.easter(today.year)
+        this_sunday = today + datetime.timedelta(6 - today.weekday())
         return ("PROPER 5", "YEAR A")
 
 
