cba55da3aba967469d7fb9a580b12f2b1725f6e0
William Wingate <wtwingate@gmail.com>
2024-04-02 17:21:31 -0400

    Add CSV file of the ACNA Lectionary

--- src/lectionary/cli.py
+++ src/lectionary/cli.py
@@ -1,20 +1,5 @@
-from lectionary.lectionary import Lectionary
-from lectionary.psalter import Psalter
-from lectionary.esv import get_esv_text
-import datetime
-
-
 def main():
-    psalter = Psalter()
-    lectionary = Lectionary()
-    today = datetime.date.today()
-    lessons = lectionary.get_lessons(today)
-    texts = []
-    texts.append(get_esv_text(lessons[0]))  # The First Lesson
-    texts.append(psalter.get_psalm(lessons[1]))  # The Psalm
-    texts.append(get_esv_text(lessons[2]))  # The Second Lesson
-    texts.append(get_esv_text(lessons[3]))  # The Gospel
-    print("\n\n".join(texts))
+    pass
 
 
 if __name__ == "__main__":
