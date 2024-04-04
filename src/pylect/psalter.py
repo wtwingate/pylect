bd9536dd6b95d3e4968637e20527c52e607da219
William Wingate <wtwingate@gmail.com>
2024-04-04 16:54:49 -0400

    Add missing documentation to Psalter class

--- src/pylect/psalter.py
+++ src/pylect/psalter.py
@@ -1,3 +1,10 @@
+"""Creates a Psalter object which imports the plain text of the Psalter from a
+PDF copy of the Book of Common Prayer 2019, cleans up the formatting, and then
+creates a nested dictionary of the Psalms by chapter, verse, and half-verse. It
+also provides convenient methods for retrieving the entire dictionary of Psalms
+or for getting a nicely formatted string of any given Psalm reference.
+"""
+
 import json
 import os
 import re
@@ -5,12 +12,18 @@ import fitz
 
 
 class Psalter:
+    """The Psalter class imports the Psalms and provides methods for retrieving them."""
+
     def __init__(self) -> None:
         self.__psalms = {}
         self.__import_psalms()
 
+    def get_psalms(self) -> dict:
+        """Return the entire Psalter as a dictionary"""
+        return self.__psalms
+
     def get_psalm(self, reference: str) -> str:
-        """Get formatted psalm text by chapter and verse reference
+        """Get formatted psalm text by chapter and verse reference.
 
         Valid references must include the "chapter" number and may include a
         a verse or range of verses. A colon ":" is used as the delimiter between
@@ -29,21 +42,21 @@ class Psalter:
         chapter, verses = self.__parse_reference(reference)
         psalm_chapter = self.__psalms.get(chapter)
         if psalm_chapter is None:
-            raise Exception("Error: invalid chapter reference")
+            raise ValueError("Error: invalid chapter reference")
         text_list.append(f"Psalm {chapter}\n")
         if verses is None:
             verses = psalm_chapter.keys()
         for verse in verses:
             psalm_verse = psalm_chapter.get(verse)
             if psalm_verse is None:
-                raise Exception("Error: invalid verse reference")
+                raise ValueError("Error: invalid verse reference")
             text_list.append(f"{verse} {psalm_verse['head']} *")
             text_list.append(f"{psalm_verse['tail']}")
         psalm_text = "\n".join(text_list)
         return psalm_text
 
     def __parse_reference(self, reference: str) -> tuple[str, list]:
-        """Make human-readable verse references computer-friendly"""
+        """Make human-readable verse references computer-friendly."""
         ref = reference.replace("Ps ", "")
         ref = ref.replace("v", "")
         if "or" in ref:
@@ -67,7 +80,7 @@ class Psalter:
         return chapter, verses
 
     def __import_psalms(self) -> None:
-        """Populate Psalter.__psalms dictionary with psalms"""
+        """Populate Psalter.__psalms dictionary with psalms."""
         if os.path.isfile("src/pylect/psalter.json"):
             self.__load_from_json()
         else:
@@ -78,12 +91,12 @@ class Psalter:
 
     def __load_from_json(self) -> None:
         """Load saved psalm dictionary"""
-        with open("src/pylect/psalter.json", "r") as json_file:
+        with open("src/pylect/psalter.json", "r", encoding="utf-8") as json_file:
             self.__psalms = json.load(json_file)
 
     def __dump_to_json(self) -> None:
         """Save psalm dictionary as a JSON file"""
-        with open("src/pylect/psalter.json", "w") as json_file:
+        with open("src/pylect/psalter.json", "w", encoding="utf-8") as json_file:
             json.dump(self.__psalms, json_file)
 
     def __get_psalter_from_pdf(self) -> str:
@@ -97,7 +110,7 @@ class Psalter:
         return full_text
 
     def __clean_up_text(self, text: str) -> str:
-        """Remove extraneous formatting from the extracted Psalter text"""
+        """Remove extraneous formatting from the extracted Psalter text."""
         lines = text.splitlines()
         clean_lines = []
         for line in lines:
@@ -110,13 +123,13 @@ class Psalter:
         return clean_text
 
     def __remove_non_ascii(self, line: str) -> str:
-        """Remove all non-ASCII characters from a string"""
+        """Remove all non-ASCII characters from a string."""
         ascii_str = line.replace("’", "'")
         ascii_str = ascii_str.replace("‘", "'")
         return "".join(c if ord(c) < 128 else " " for c in ascii_str)
 
     def __remove_header_footer(self, line: str) -> str:
-        """Remove headers, footers, and page numbers from Psalter text"""
+        """Remove headers, footers, and page numbers from Psalter text."""
         mp_header = re.compile(
             r".*(m\s*o\s*r\s*n\s*i\s*n\s*g\s+p\s*r\s*a\s*y\s*e\s*r).*"
         )
@@ -135,7 +148,7 @@ class Psalter:
         return line
 
     def __text_to_psalms(self, text: str) -> None:
-        """Process cleaned-up Psalter text into a dictionary of Psalm objects"""
+        """Process cleaned-up Psalter text into a dictionary of Psalm objects."""
         psalms = self.__split_psalms(text)
         for index, psalm in enumerate(psalms):
             ps_chapter = str(index + 1)
@@ -145,7 +158,7 @@ class Psalter:
             self.__psalms[ps_chapter] = ps_verses
 
     def __split_psalms(self, text: str) -> list:
-        """Split psalms into separate chapters"""
+        """Split psalms into separate chapters."""
         ps_number = re.compile(r"\n*\d+\n")
         psalms = re.split(ps_number, text)
         while "" in psalms:
@@ -154,7 +167,7 @@ class Psalter:
         return psalms
 
     def __remove_psalm_119_titles(self, psalm: str) -> str:
-        """Remove Hebrew and Latin titles throughout Psalm 119"""
+        """Remove Hebrew and Latin titles throughout Psalm 119."""
         psalm_no_titles = psalm
         titles = [
             re.compile(r".*\nAleph\n"),
@@ -185,7 +198,7 @@ class Psalter:
         return psalm_no_titles
 
     def __get_psalm_verses(self, psalm: str) -> dict:
-        """Split psalms into separate verses and half-verses"""
+        """Split psalms into separate verses and half-verses."""
         vs_number = re.compile(r"\d+")
         asterisk = re.compile(r"\s*\*\s*")
         verses = re.split(vs_number, psalm)
@@ -202,7 +215,7 @@ class Psalter:
         return split_verses
 
     def __clean_up_verses(self, verses: list) -> str:
-        """Remove extraneous formatting within individual verses"""
+        """Remove extraneous formatting within individual verses."""
         clean_verses = []
         for verse in verses:
             clean_verse = verse.strip()
