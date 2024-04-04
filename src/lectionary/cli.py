84e17ac2a579850646a7e35fc07bc73a919bea75
William Wingate <wtwingate@gmail.com>
2024-04-03 23:06:37 -0400

    Allow arbitrary start and end dates for CLI tool

--- src/lectionary/cli.py
+++ src/lectionary/cli.py
@@ -6,20 +6,22 @@ from lectionary.psalter import Psalter
 
 
 class CLI:
-    def __init__(self, date: dt.date | None = None) -> None:
+    def __init__(
+        self, start_date: dt.date | None = None, end_date: dt.date | None = None
+    ) -> None:
         self.lectionary = Lectionary()
         self.psalter = Psalter()
-        self.today = date if date is not None else dt.date.today()
-        self.sunday = self.__get_sunday()
+        self.start_date = start_date if start_date is not None else dt.date.today()
+        self.end_date = end_date if end_date is not None else self.__get_sunday()
 
     def __get_sunday(self) -> dt.date:
         return self.today + dt.timedelta(days=6 - self.today.weekday())
 
     def get_holy_days_from_lectionary(self) -> list:
         dates = []
-        current_day = self.today
+        current_day = self.start_date
         day_delta = dt.timedelta(days=1)
-        while current_day <= self.sunday:
+        while current_day <= self.end_date:
             dates.append(current_day)
             current_day += day_delta
         holy_days = self.lectionary.get_holy_days(dates)
@@ -28,7 +30,7 @@ class CLI:
 
 def main():
     # TODO: Print the titles and lectionary references for these dates
-    cli = CLI(dt.date(2023, 12, 25))
+    cli = CLI(dt.date(2024, 4, 1), dt.date(2024, 6, 1))
     holy_days = cli.get_holy_days_from_lectionary()
     for day in holy_days:
         print(day.name)
